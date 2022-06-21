5fda6d16be0a4dd49058eb92d894fa4a46ffdb39
RaulFigs <92529545+RaulFigs@users.noreply.github.com>
2022-06-22 00:31:49 +0100

    Update_18.2

--- app.py
+++ app.py
@@ -80,10 +80,8 @@ def login():
         conn.close()
     except (Exception, psycopg2.DatabaseError):
         return jsonify({"Erro": "Utilizador não encontrado"}), NOT_FOUND_CODE
-
-    return "here"
     
-    return {"Message":"Login realizado com sucesso!", "Token": token.decsode('utf-8')}, OK_CODE
+    return jsonify({"Message": "Login realizado com sucesso!", "Token": token.decsode('utf-8')}), OK_CODE
 
 
 ## REGISTO
