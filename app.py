615375b2605479ad1671d62aacf8b33ba05d2d2c
BeaSimoess <92529749+BeaSimoess@users.noreply.github.com>
2022-06-25 18:04:44 +0100

    Desculpa raul por não estar a meter os nomes direitos

--- app.py
+++ app.py
@@ -226,10 +226,12 @@ def removerLista():
 
     query = """DELETE FROM lista WHERE id = %s;"""
 
+    values = content
+
     try:
         with db_connection() as conn:
             with conn.cursor() as cursor:
-                cursor.execute(query, content)
+                cursor.execute(query, values)
         conn.close()
     except (Exception, psycopg2.DatabaseError):
         return jsonify({"message": "A Lista não foi removida!"}), NOT_FOUND_CODE
